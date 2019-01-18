b59d94ca7953b0522b96e2d5972abc7e7f7d03a8
rafal.slosarz <rafalslosarz@gmail.com>
2019-01-18 20:28:45 +0100

    DI

--- lib/utils/injection_init.dart
+++ lib/utils/injection_init.dart
@@ -1,13 +1,21 @@
 import 'package:airadar/blocks/place_block.dart';
+import 'package:airadar/blocks/weather_block.dart';
 import 'package:airadar/repo/place_repository.dart';
 import 'package:airadar/repo/place_service.dart';
+import 'package:airadar/repo/weather_repository.dart';
+import 'package:airadar/repo/weather_service.dart';
 import 'package:flutter_simple_dependency_injection/injector.dart';
 
 class InjectionInit {
   static void init() {
     final injector = Injector.getInjector();
+    injector.map<WeatherRepository>((i) => new WeatherService(),
+        isSingleton: true);
+    injector.map<WeatherBlock>(
+        (i) => new WeatherBlock(injector.get<WeatherRepository>()));
     injector.map<PlaceRepository>((i) => new PlaceService(), isSingleton: true);
-    injector.map<PlaceBlock>((i) => new PlaceBlock(injector.get<PlaceRepository>()));
+    injector.map<PlaceBlock>(
+        (i) => new PlaceBlock(injector.get<PlaceRepository>()));
   }
 
   static void dispose() {
